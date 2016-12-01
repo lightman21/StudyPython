a5a3ab73aaee9ce5f6d32867ad600bc6bbeafa3a
tanghao <tangxiansheng21@gmail.com>
2016-12-01 12:55:44 +0800

    python day 9,and 加油^_^

---
+++ quiz/GeneratNumber.py
@@ -0,0 +1,10 @@
+'''
+Create a function generateNumbers(start, end) that takes in two numbers as arguments
+and returns a list of numbers starting from start to the end number (inclusive)
+specified in the arguments.
+Note: The function range(x, y) can takes in 2 arguments.
+For example, range(1, 5) will return a list of numbers [1,2,3,4].
+'''
+def generateNumber(start,end):
+    if start == end
+        return start
