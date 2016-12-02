2d71797d647eab347ab137b138d1e8cf4aa6ee10
tanghao <tangxiansheng21@gmail.com>
2016-12-02 08:01:51 +0800

    day 10 read the portfolio as a list write Python list to json read json to Python list

--- quiz/GeneratNumber.py
+++ quiz/GeneratNumber.py
@@ -5,6 +5,27 @@ specified in the arguments.
 Note: The function range(x, y) can takes in 2 arguments.
 For example, range(1, 5) will return a list of numbers [1,2,3,4].
 '''
+import sys
+
 def generateNumber(start,end):
-    if start == end
-        return start
+
+    if start >= end:
+        start = end
+
+    if start == end:
+        return  range(start,start+1)
+
+    return range(start,end+1)
+
+print(len(sys.argv))
+
+if len(sys.argv) != 3 :
+    print('Shit Happend')
+else:
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    print(start,end)
+    generateNumber(start,end)
+
+
+
