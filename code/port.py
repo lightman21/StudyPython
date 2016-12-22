8b1cd3e38c73abc1b711b632266e7d7584c44181
tanghao <tangxiansheng21@gmail.com>
2016-12-22 23:00:01 +0800

    General Purpose CSV Parsing Module

--- code/port.py
+++ code/port.py
@@ -1,19 +1,35 @@
 # port.py
+import reader
 
 #f = open('data/portfolio.csv','r')
 
-total = 0.0
-with open('data/portfolio.csv','r') as f:
-    #caution: skip a single of input (they are titles)
-    headers = next(f)
-    for line in f:
-        line = line.strip()     # Strip whitespace
-        parts = line.split(',')
-        parts[0] = parts[0].strip('"')
-        parts[1] = parts[1].strip('"')
-        parts[2] = int(parts[2])
-        parts[3] = float(parts[3])
-        total += parts[2] * parts[3]
-        print(parts)
+#total = 0.0
+#with open('data/portfolio.csv','r') as f:
+#    #caution: skip a single of input (they are titles)
+#    headers = next(f)
+#    for line in f:
+#        line = line.strip()     # Strip whitespace
+#        parts = line.split(',')
+#        parts[0] = parts[0].strip('"')
+#        parts[1] = parts[1].strip('"')
+#        parts[2] = int(parts[2])
+#        parts[3] = float(parts[3])
+#        total += parts[2] * parts[3]
+#        print(parts)
+#
+#print('total is:',total)
 
-print('total is:',total)
+def read_portfolio(filename,*, errors='warn'):
+    '''
+    Read a CSV file with name,data,shares,price data into a list
+    '''
+    return reader.read_csv(filename,[str, str, int, float], errors=errors)
+
+if __name__ == '__main__':
+    portfolio = read_portfolio('../data/portfolio.csv')
+
+    total = 0.0
+    for holding in portfolio:
+        total += holding['shares'] * holding['price']
+
+    print('Total costs:', total)
---
+++ code/portie/port.py
@@ -0,0 +1,17 @@
+# port.py
+from . import reader    # Package-relative import
+
+def read_portfolio(filename,*,errors='warns'):
+    '''
+    Read a CSV file with name,date,shares,price data into a list
+    '''
+    return portie.reader.read_csv(filename,[str,str,int,float],errors=errors)
+
+if __name__ == '__main__':
+    portfolio = read_portfolio('Data/portfolio.csv')
+
+    total = 0.0
+    for holding in portfolio:
+        total += holding['shares'] * holding['price']
+
+    print('Total cost:',total)
