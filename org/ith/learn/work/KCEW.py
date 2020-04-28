25d51358fbd53218c157e4c4c049a4a53af2e121
tanghao <tanghao@keruyun.com>
2020-04-28 23:39:53 +0800

    check default biggest

--- org/ith/learn/work/KCEW.py
+++ org/ith/learn/work/KCEW.py
@@ -12,15 +12,60 @@ import re
 import difflib
 
 
-def main():
-    path_of_search = '/Users/lightman_mac/company/keruyun/oh_my_python/StudyPython/org/ith/learn/scratch/tmp' \
-                     '/auto_extract_work/2020_04_27/'
+def check_default_biggest():
+    path_of_search = '/tmp/auto_extract/'
+    count = 0
+    path_dict = dict()
     for dir_path_name, dirs, files in os.walk(path_of_search):
+        files = sorted(files)
+        for single in files:
+            full_path = dir_path_name + single
+            kce_list = read_xml_as_kce_list(full_path)
+            count += 1
+            string = '{:<105} ,size {:<10}'.format(full_path, len(kce_list))
+            simple = full_path.split('___')[0]
+            path_dict[full_path] = simple
+            print(string)
 
-        print(files)
+    print('total count ', count)
+    aar_path_list_dict = dict()
+    for keypath, aar in path_dict.items():
+        list_of_path = list()
+        if aar in aar_path_list_dict.keys():
+            list_of_path = aar_path_list_dict.get(aar)
+        list_of_path.append(keypath)
+        aar_path_list_dict[aar] = list_of_path
+
+    print('aar_path_list_dict size ', len(aar_path_list_dict))
+
+    for aar, values_list in aar_path_list_dict.items():
+        deal_single(aar, values_list)
 
 
-        pass
+def deal_single(aar, values_list):
+    def_flag = '___values.xml'
+    list_of_keyset = []
+    def_keyset = list()
+    for value in values_list:
+        list_kce = read_xml_as_kce_list(value)
+        keyset = set()
+        for kce in list_kce:
+            keyset.add(kce.key)
+        keyset = sorted(keyset)
+        if str(value).endswith(def_flag):
+            def_keyset = keyset
+        list_of_keyset.append(keyset)
+
+    for keys in list_of_keyset:
+        for key in keys:
+            if key not in def_keyset:
+                ss = '{:<80}  default is not biggest'.format(aar)
+                print(ss)
+                return False
+
+
+def main():
+    check_default_biggest()
 
 
 if __name__ == '__main__':
